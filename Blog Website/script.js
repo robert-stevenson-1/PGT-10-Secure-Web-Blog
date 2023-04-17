917b4b32e013e1462bfb442f4abbfa20707636ed
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-04-17 13:06:31 +0100

    Logged in checks templated
    - Login/signup buttons are added if 'isLoggedIn()' returns true to the (main/top) nav
    - added options to the posts for deleting and editing posts (HTML ONLY)
    - the options to delete and edit on the posts are added if 'isLoggedIn()' returns true (HTML ONLY)
    - isLoggedIn() still need to connecting to the server backend to determine if you are logged in

--- Blog Website/script.js
+++ Blog Website/script.js
@@ -3,11 +3,18 @@ window.onload = onload()
 
 function onload() {
     console.log('Page loaded');
+
+    if(!isLoggedIn()) {
+        addLoginSignupButtons()
+    }
+
     // make a GET request for the post in the database on the server
     fetch('/get_posts',
     {
         method: 'GET',
     }).then(function(response){ //with the response....
+        console.error("Status: " + response.status);
+        console.error(response);
         return response.json() // return the JSON of the response
     })
     .then(data => processPosts(data))
@@ -45,14 +52,18 @@ function generatePostTemplate(post_title, post_body, post_user, post_date){
     // structure all the post's elements
     div_post_container.appendChild(div_center);
     div_post_container.appendChild(div_post_info);
+
     div_center.appendChild(h3_post_title);
     div_center.appendChild(p_post_body);
+
     div_post_info.appendChild(div_post_user);
     div_post_info.appendChild(div_post_date);
 
     //add the style tags to the HTML elements
     div_post_container.classList.add('container-posts');
+    
     div_center.classList.add('container-center')
+
     div_post_user.classList.add('left-aligned')
     div_post_date.classList.add('right-aligned')
 
@@ -62,7 +73,78 @@ function generatePostTemplate(post_title, post_body, post_user, post_date){
     div_post_date.textContent = "Date Posted: " + post_date;
     p_post_body.textContent = post_body;
 
+    if (isLoggedIn()) {
+        // create options for the post if we in a logged in session
+        var div_post_option = document.createElement('div');
+        div_post_info.appendChild(div_post_option);
+        div_post_option.classList.add('container-center')
+
+        // delete option
+        // TODO: add click event handler
+        var btn_delete_post = document.createElement('a');
+        var fa_delete_post = document.createElement('i');
+        
+        div_post_option.appendChild(btn_delete_post);
+        btn_delete_post.appendChild(fa_delete_post);
+        
+        btn_delete_post.classList.add('nav-button');
+        fa_delete_post.classList.add('fa');
+        fa_delete_post.classList.add('fa-trash');
+        
+        btn_delete_post.href = "#";
+        
+        // edit option
+        // TODO: add click event handler
+        var btn_edit_post = document.createElement('a');
+        var fa_edit_post = document.createElement('i');
+        
+        div_post_option.appendChild(btn_edit_post);
+        btn_edit_post.appendChild(fa_edit_post);
+        
+        btn_edit_post.classList.add('nav-button');
+        fa_edit_post.classList.add('fa');
+        fa_edit_post.classList.add('fa-pencil');
+
+        btn_edit_post.href = "#";
+    }
+    
     //return the created post html object
     return div_post_container
+}
 
+function isLoggedIn() {
+    // TODO: check with the server if we are logged in or not
+
+    //return true if we are logged in
+    return true;
+
+    // not logged in
+    // return false;
+}
+
+function addLoginSignupButtons(){
+    // <a href="LoginSignup.html" class="nav-button left-aligned LoginSignup"><i class="">Log in</i></a>
+    // <a href="LoginSignup.html" class="nav-button left-aligned LoginSignup"><i class="">Sign Up</i></a>
+    main_nav = document.getElementById('nav-main');
+    
+    console.log(main_nav);
+
+    btn_login = document.createElement('a');
+    btn_login_content = document.createElement('i');
+    btn_login.appendChild(btn_login_content);
+    btn_login_content.textContent = "Log in";
+    btn_login.href = "LoginSignup.html";
+    btn_login.classList.add('nav-button');
+    btn_login.classList.add('left-aligned');
+    
+    btn_signup = document.createElement('a');
+    btn_signup_content = document.createElement('i');
+    btn_signup.appendChild(btn_signup_content);
+    btn_signup_content.textContent = "Sign Up";
+    btn_signup.href = "LoginSignup.html";
+    btn_signup.classList.add('nav-button');
+    btn_signup.classList.add('left-aligned');
+
+    main_nav.appendChild(btn_login);
+    main_nav.appendChild(btn_signup);
 }
