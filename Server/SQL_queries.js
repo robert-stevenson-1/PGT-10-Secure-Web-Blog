329bfe94cbb49ef2687a5006fdbf9e23c3c25cf5
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-04-25 23:18:44 +0100

    Update SQL_queries.js

--- Server/SQL_queries.js
+++ Server/SQL_queries.js
@@ -4,6 +4,8 @@
 
 // get all the posts from the database
 const GET_ALL_POSTS = 'SELECT accounts.username, posts.title, posts.content, posts.created_at FROM posts, accounts WHERE posts.account_id = accounts.ID';
+// get all the posts from the database for a specific user
+const GET_A_USERS_POSTS = 'SELECT accounts.username, posts.title, posts.content, posts.created_at FROM posts, accounts WHERE posts.account_id = accounts.ID and accounts.ID == $1';
 //look for user who has a matching Username and password
 const GET_USER = 'SELECT id, username, email FROM accounts WHERE username = $1 AND password = $2';
 // check if the a user exists in the database. Returns 1 is user exists and 0 is they don't exist.
