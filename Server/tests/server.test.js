c559f9df4c04b2afca0026abefe3a72764a3da30
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-04-28 12:22:33 +0100

    Prep for Unit Tests

---
+++ Server/tests/server.test.js
@@ -0,0 +1,34 @@
+import '../server.js';
+
+// guide for chai style differences: https://www.chaijs.com/guide/styles/
+var assert = require('chai').assert,
+    expect = require('chai').expect;
+
+// TODO: Test we can check that a user that is in the db is found and is allowed to login
+describe("Test we can check that a user that is in the db is found and is allowed to login", function(){
+
+});
+// TODO: Test we can check that a user that is NOT in the db is rejected from logging in
+describe("Test we can check that a user that is NOT in the db is rejected from logging in", function (){
+
+});
+// TODO: Test we can retrieve the posts from the database
+describe("Test we can retrieve the posts from the database", function(){
+
+});
+// TODO: Test we can retrieve a user from the database
+describe("Test we can retrieve a user from the database", function(){
+
+});
+// TODO: Test we can retrieve a single, specific post from the database
+describe("Test we can retrieve a single, specific post from the database", function(){
+
+});
+// TODO: Test we can search the database and get the result
+describe("Test we can search the database and get the result", function(){
+
+});
+// TODO: Test blocking/preventing of sql injection
+describe("Test blocking/preventing of sql injection", function(){
+
+});
